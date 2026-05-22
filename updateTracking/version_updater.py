ea0a198bab26cfc3856188befc8a94ed8ca41064
Matthew Fierek <mjf3464@gmail.com>
2026-05-22 08:54:58 -0400

    Made icon generation dynamic

--- updateTracking/version_updater.py
+++ updateTracking/version_updater.py
@@ -18,6 +18,7 @@ MANIFEST_URLS = [
 
 RENDERCHEST_EXE = Path("/home/taghost/MCDisplayEditorDocker/renderchest/renderchest")
 
+
 ASSET_DIRS_TO_COPY = {
     "blockstates": "assets/minecraft/blockstates",
     "models/block": "assets/minecraft/models/block",
